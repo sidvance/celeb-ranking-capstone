c1ec42d8d9c2730c71da9ae9a43f9c0822974270
Cassidy <cassidy.vance@hotmail.com>
2023-04-13 22:32:20 -0700

    added front end response handling

--- Public/index.js
+++ Public/index.js
@@ -1,3 +1,54 @@
 //axios endpoint requests and response handling. Listening for requests sent from backend
 
-const baseURL = 'http://localhost:4646/'
+const baseURL = 'http://localhost:4646'
+
+//Interactive js. Grab html element. Write fn. Add event listener.
+
+let chooseBtn = document.querySelector('#chooseBtn')
+let randomBtn = document.querySelector('#randomBtn')
+let addBtn = document.querySelector('#addBtn')
+let mainSection = document.querySelector('#mainSection')
+
+
+const getAllActors = () => {
+    axios.get(`${baseURL}/allActors`)
+        .then((res) => {
+            console.log(res.data)
+        })
+        .catch((theseHands) => {
+            console.log(theseHands)
+        })
+}
+
+const getRandomActor = () => {
+    axios.get(`${baseURL}/actor`)
+    .then((res) => {
+        console.log(res.data)
+    })
+    .catch((theseHands) => {
+        console.log(theseHands)
+    })
+}
+
+const addActor = () => {
+    axios.post(`${baseURL}/allActors`)
+    .then((res) => {
+        console.log(res.data)
+        //loop over res.data and create input fields for each property
+        // for(let i = 0; i < res.data.length; i++){
+        //     let pTag = document.createElement('p')
+        //     pTag.innerHTML = `
+        //         ${res.data[i]}: <input type="text">
+        //     `
+        //     mainSection.appendChild(pTag)
+        // }
+    })
+    .catch((theseHands) => {
+        console.log(theseHands)
+    })
+}
+
+
+chooseBtn.addEventListener('click', getAllActors)
+randomBtn.addEventListener('click', getRandomActor)
+addBtn.addEventListener('click', addActor)
